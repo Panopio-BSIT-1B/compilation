545bb71318891adefb421f7999af533cdaf55621
PANOPIO_BSIT1B <alypanopio.pat@gmail.com>
2025-08-27 15:08:44 +0800

    commit lalalala

---
+++ activity10.py
@@ -0,0 +1,11 @@
+username = "lybog_user"
+password = "babykosikulot69"
+
+uname = input("Enter username --> ")
+pword = input("Enter password --> ")
+
+if uname == username and pword == password :
+	print("ACCESS GRANTED")
+else:
+	print("ACCESS DENINED")
+.
