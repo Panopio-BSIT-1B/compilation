b841ce25665f041ff009ae8c382fe9fedda0c99b
PANOPIO_BSIT1B <alypanopio.pat@gmail.com>
2025-09-16 10:54:14 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,8 @@
+
+username = "lybog123456"
+password = "walaakongpake"
+username = input("Please Enter your username: ")
+password = input("Please Enter your Password: ")
+
+print(( username== "lybog123456" and password == "walakongpake" ))
+
