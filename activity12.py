4785fd7538cff2d295de38aaa7c190d4aacd7816
PANOPIO_BSIT1B <alypanopio.pat@gmail.com>
2025-09-16 11:11:08 +0800

    Update activity12.py

--- activity12.py
+++ activity12.py
@@ -1,22 +1,2 @@
-#print hello word 10 times
-
-#for tt in range (1, 11, 1):
-#    print(tt, "times")
-
-#mine
-
-#for my in range (1, 11, 3):
-#    print(my, "times")
-
-#walang start step
-
-#for lola in range (110):
-#    print(lola, "times")
-
-sum = 0
-for Franz in range(1, 11, 1):
-    number = eval(input("enter any num: "))
-    Franz + 1
-    sum += number
-
-print("The total of all the numbers is", sum)
+for baby in range(1,21,1):
+    print(baby,"Franz")
