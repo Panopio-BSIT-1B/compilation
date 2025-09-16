a7eb154716b963aa6dbbd518dc02ec544400e840
PANOPIO_BSIT1B <alypanopio.pat@gmail.com>
2025-09-16 10:42:45 +0800

    activity7.py

---
+++ activity7.py
@@ -0,0 +1,20 @@
+A = 4
+
+print("The value of a is", A)
+
+A += 4
+A -= 5
+A *= 3
+A += 2
+
+print("The value of a is", A)
+
+B = 7
+
+print("The value of b is", B)
+
+B += 7
+B *= 7
+B -= 7
+
+print("The value of b is", B)
