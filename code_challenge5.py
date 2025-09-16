5ce94ce8969bdf97ba1f6cef6762b33b12f78272
PANOPIO_BSIT1B <alypanopio.pat@gmail.com>
2025-09-16 10:27:37 +0800

    Delete code_challenge5.py

--- code_challenge5.py
+++
@@ -1,8 +0,0 @@
-
-
-sum = 0
-for Value in range(8):
-    love_value = eval(input("Place any value here: "))
-    if love_value % 2 != 0:
-        sum = sum + love_value
-print("The sum of the odd number is:", sum)
