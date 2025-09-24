1493512c2263b57e529e8ece28d59e0c1fd94a23
PANOPIO_BSIT1B <alypanopio.pat@gmail.com>
2025-09-24 15:02:28 +0800

    commit

---
+++ activity15.py
@@ -0,0 +1,14 @@
+fname = "Alfredo Jr."
+mname = "Gonzales"
+lname = "Panopio"
+
+print(f"My full name is {fname} {mname} {lname}")
+
+
+
+
+fname = "Alfredo Jr."
+mname = "Gonzales"
+lname = "Panopio"
+
+print(f"My full name is {fname.upper()} {mname.upper()} {lname.upper()}")
