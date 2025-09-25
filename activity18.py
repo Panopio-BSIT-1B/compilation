018af2b8d7dd4332c2ae80b3bbf1228f5de3f270
PANOPIO_BSIT1B <alypanopio.pat@gmail.com>
2025-09-25 20:38:19 +0800

    commit

---
+++ activity18.py
@@ -0,0 +1,10 @@
+for i in range(1,11,1):
+    for x in range(1, i ,1): 
+        print(x, end=" ")
+    print()
+
+
+for i in range(1,31,1):
+    for x in range(1, i ,1):
+        print("*", end=" ") 
+    print()
