b397bf430b0c667e2f61bd679695c109b025e9f9
PANOPIO_BSIT1B <alypanopio.pat@gmail.com>
2025-10-27 14:40:03 +0800

    commit

---
+++ code_challlenge14.py
@@ -0,0 +1,14 @@
+list_anime = []
+
+while True:
+    anime = str(input("Enter a name of an anime (or type 'exit' to finish): "))
+    if anime == 'exit':
+        print("\nYour anime list includes: ")
+        for a in list_anime:
+            print(f"- {a}")
+        break
+    else:
+        list_anime.append(anime)
+        print(f"{anime} has been added to your list.")
+        continue
+    
